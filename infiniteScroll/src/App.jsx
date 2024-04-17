f528932b53302e36e9efcd66d5db34e44d7800d2
MK884 <marchantkhalid7810@gmail.com>
2024-04-17 12:40:08 +0530

    react optimization for infinity scrolling

--- infiniteScroll/src/App.jsx
+++ infiniteScroll/src/App.jsx
@@ -1,44 +1,60 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(25)
+  const [count, setCount] = useState(28);
 
   const elements = [];
   for (let index = 1; index <= count; index++) {
     elements.push(index);
   }
 
-  useEffect(()=>{
-
-    const loadData = ()=>{
-      if(window.innerHeight+ window.scrollY >= window.document.body.offsetHeight-30){
-        setCount((prev)=>prev+30);
+  useEffect(() => {
+    const loadData = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        window.document.body.offsetHeight - 10
+      ) {
+        setCount((prev) => prev + 28);
       }
-    }
+    };
 
-    document.addEventListener('scroll',loadData)
+    document.addEventListener("scroll", loadData);
 
-    return ()=> document.addEventListener('scroll',loadData)
-  },[count])
+    return () => document.addEventListener("scroll", loadData);
+  }, [count]);
 
   return (
     <>
-    <div style={{
-      color: 'white',
-      display: 'flex',
-      flexDirection: 'column',
-    }}>
-      {
-        elements.map((e,id)=>(
-          <p key={id}>{e}</p>
-        ))
-      }
+      <div
+        style={{
+          color: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          gap: "20px",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {elements.map((e, id) => (
+          <div
+            key={id}
+            style={{
+              backgroundColor: "#131313",
+              height: "300px",
+              width: "400px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h1>{e}</h1>
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
